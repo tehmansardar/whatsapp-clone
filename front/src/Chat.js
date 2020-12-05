ea1c5e0fe8199d3ebe160ef2260097e5113eed5a
Tehman Sardar <tehmansardar@gmail.com>
2020-12-05 14:48:38 +0500

    Project Layout Structure Styled

---
+++ front/src/Chat.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Chat.css';
+const Chat = () => {
+    return (
+        <div className="chat">
+            <h1>I'm Chat</h1>
+        </div>
+    )
+}
+
+export default Chat
