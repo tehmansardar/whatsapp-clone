36e063315e553ace56afb12ae6185bee1585a32f
Tehman Sardar <tehmansardar@gmail.com>
2020-12-10 03:37:14 +0500

    Chat Header Design

--- front/src/Chat.js
+++ front/src/Chat.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import { Avatar, IconButton } from '@material-ui/core';
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
+import AttachFileOutlinedIcon from '@material-ui/icons/AttachFileOutlined';
+import MoreVertOutlinedIcon from '@material-ui/icons/MoreVertOutlined';
 import './Chat.css';
 const Chat = () => {
     return (
         <div className="chat">
-            <h1>I'm Chat</h1>
+            <div className="chat__header">
+                <Avatar/>
+                <div className="chat__headerInfo">
+                    <h3>Room name</h3>
+                    <p>last seen</p>
+                </div>
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <SearchOutlinedIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <AttachFileOutlinedIcon/>
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertOutlinedIcon/>
+                    </IconButton>
+                </div>
+            </div>
         </div>
     )
 }
