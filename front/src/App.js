c2d5ed99533f557b2af69c1f4f89615ef35a11db
Tehman Sardar <tehmansardar@gmail.com>
2020-12-11 03:08:40 +0500

    Pusher Integrated to the fronted

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,22 @@
+import {useEffect} from 'react'
+import Pusher from 'pusher-js'
 import Sidebar from './Sidebar'
 import Chat from './Chat'
 
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const pusher = new Pusher('9e0c40915bb55ba90506', {
+      cluster: 'ap2'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', data => {
+        alert(JSON.stringify(data));
+    });
+
+  }, [])
   return (
     <div className="App">
       <div className="app__body">
