2f6b501020df9d5f836019938aa9c1edcb2c9fd1
Tehman Sardar <tehmansardar@gmail.com>
2020-12-10 22:54:44 +0500

    Message Schema Design

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 // importing
 import express from 'express';
 import mongoose from 'mongoose'
+import Messages from './dbMessages.js'
 // app config
 const app =  express();
 const port = process.env.PORT || 9000;
@@ -19,6 +20,7 @@ const db = mongoose.connection;
 db.once('open', ()=>{
     console.log('DB connected');
 })
+
 ///??????
 
 // api routes
