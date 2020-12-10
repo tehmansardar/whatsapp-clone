6d71cc10259fb8136452ce8ff367cbb19d07d376
Tehman Sardar <tehmansardar@gmail.com>
2020-12-11 01:48:52 +0500

    Post Message

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 9000;
 
 
 // middleware
+app.use(express.json())
 
 // DBconfig
 const connection_url = 'mongodb+srv://admin:VTBfuQ5lkLrUSumM@cluster0.nhuw8.mongodb.net/whatsappdb?retryWrites=true&w=majority'
@@ -25,6 +26,17 @@ db.once('open', ()=>{
 
 // api routes
 app.get('/', (req,res)=> res.status(200).send('Hello world'))
+
+app.post('/messages/new', (req,res)=>{
+    const dbMessages = req.body;
+    Messages.create(dbMessages,(err,data)=>{
+        if(err){
+            res.status(500).send(err)
+        }else{
+            res.status(201).send(data)
+        }
+    })
+})
 
 // listen
 app.listen(port, ()=>console.log(`Running on ${port}`))
